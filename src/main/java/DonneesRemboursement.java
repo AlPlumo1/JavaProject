a72e94b9e7516d2b993e3fd54296c6c7688ce3b7
AlPlumo1 <theo.demars043@gmail.com>
2023-05-31 16:19:53 +0200

    4th commit : The one you can clone to be up to date (31/05/2023)

---
+++ src/main/java/DonneesRemboursement.java
@@ -0,0 +1,28 @@
+class DonneesRemboursement {
+    private String numeroSecuriteSociale;
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String numeroTelephone;
+    private String email;
+    private String idRemboursement;
+    private String codeSoin;
+    private double montantRemboursement;
+
+    // Constructeur, getters et setters
+
+    @Override
+    public String toString() {
+        return "DonneesRemboursement{" +
+                "numeroSecuriteSociale='" + numeroSecuriteSociale + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance='" + dateNaissance + '\'' +
+                ", numeroTelephone='" + numeroTelephone + '\'' +
+                ", email='" + email + '\'' +
+                ", idRemboursement='" + idRemboursement + '\'' +
+                ", codeSoin='" + codeSoin + '\'' +
+                ", montantRemboursement=" + montantRemboursement +
+                '}';
+    }
+}
---
+++ src/main/java/Parser.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class Parser {
+
+    private String filePath;
+    public Parser(String filePath) {
+       this.filePath = filePath;
+    }
+
+    public List<DonneesRemboursement> parse() {
+        List<DonneesRemboursement> donneesRemboursementList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                if (fields.length == 9) {
+                    String numeroSecuriteSociale = fields[0].trim();
+                    String nom = fields[1].trim();
+                    String prenom = fields[2].trim();
+                    String dateNaissance = fields[3].trim();
+                    String numeroTelephone = fields[4].trim();
+                    String email = fields[5].trim();
+                    String idRemboursement = fields[6].trim();
+                    String codeSoin = fields[7].trim();
+                    double montantRemboursement = Double.parseDouble(fields[8].trim());
+
+                    DonneesRemboursement donneesRemboursement = new DonneesRemboursement();
+                    donneesRemboursementList.add(donneesRemboursement);
+                } else {
+                    System.out.println("Erreur : ligne mal formatée - " + line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return donneesRemboursementList;
+    }
+}
+
